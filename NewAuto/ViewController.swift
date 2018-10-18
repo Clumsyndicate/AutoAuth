4e6c2ffbf70a79597c7ab768fed9dbf762fe38cf
Clumsyndicate <johnson.zhou.sh@icloud.com>
2018-10-18 15:58:43 +0800

    Initial Commit

---
+++ NewAuto/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NewAuto
+//
+//  Created by Johnson Zhou on 18/10/2018.
+//  Copyright © 2018 Johnson Zhou. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
